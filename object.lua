3f42d6accced2b2e70cda59164e40bb4add8d905
Jiri Kratochvil <kratochvil@klok.cz>
2014-01-02 21:56:10 +0100

    * performance optimalization

--- object.lua
+++ object.lua
@@ -10,9 +10,30 @@ local overridable_metamethods = {
   '__eq', '__lt', '__le',
 }
 
-local camelize = function(str)
-    return str:gsub("^%l", string.upper)
+local s_upper = string.upper
+local s_lower = string.lower
+local s_gsub  = string.gsub
+local s_match = string.match
+
+local function memoize (f)
+  local mem = {}
+  setmetatable(mem, {__mode = "kv"})
+  return function (x)
+    local r = mem[x]
+    if r == nil then
+      r = f(x)
+      mem[x] = r
+    end
+    return r
+  end
 end
+
+
+local camelize = function(str)
+    return s_gsub(str,"^%l", s_upper)
+end
+
+camelize = memoize(camelize)
 
 local util = {
   toGetter = function(property)
@@ -24,25 +45,34 @@ local util = {
   end,
 
   toProperty = function(method)
-    return method:gsub("^[sg]et",""):gsub("^%a", string.lower)
+    return s_gsub(s_gsub(method,"^[sg]et",""),"^%a", s_lower)
   end,
 
   isSetter = function(method)
-    return method:match("^set.+")
+    return s_match(method,"^set.+")
   end,
 }
 
 local object_reflection = function(obj)
   return { -- todo cache results
     obj = obj,
+
+    has = function(self, index)
+      return self.obj[index] ~= nil
+    end,
+
     hasProperty = function(self, property)
-      local e,p = pcall(function() return self.obj[property] end)
-      return e and p ~= nil and type(p) ~= 'function'
+      --local e,p = pcall(function() return self.obj[property] end)
+      --return e and p ~= nil and type(p) ~= 'function'
+      local p = self.obj[property]
+      return p ~= nil and type(p) ~= 'function'
     end,
 
     hasMethod = function(self, method)
-      local e,m = pcall(function() return self.obj[method] end)
-      return e and m ~= nil and type(m) == 'function'
+      --local e,m = pcall(function() return self.obj[method] end)
+      --return e and m ~= nil and type(m) == 'function'
+      local m = self.obj[method]
+      return m ~= nil and type(m) == 'function'
     end,
 
     hasGetter = function(self, property) 
@@ -62,9 +92,10 @@ end
 
 local object_manipulators = function(instance, strict)
   if strict == nil then strict = true end
+
   local proxy = getmetatable(instance)
   local impl = proxy and proxy.impl or nil
-  local rx = object_reflection(impl)
+  local rx = proxy and proxy.reflection or nil
 
   if strict and (not proxy or not impl or not rx) then
     error('Is not proxy object: '..tostring(instance))
@@ -72,6 +103,59 @@ local object_manipulators = function(instance, strict)
 
   return proxy, impl, rx
 end
+
+
+
+    local M__newindex = function(instance, index, value)
+      local proxy, impl, rx = object_manipulators(instance) 
+      --print("W:", instance, proxy, impl, index, type(impl[index]), value)
+      if rx:hasSetter(index) then
+        --print("S")
+        local setter = util.toSetter(index)
+        impl[setter](impl,value)
+        return
+      elseif impl[index] ~= nil then -- get direct property/method
+        --print("D")
+        impl[index] = value
+        return 
+      end
+      error("write unknown attribute: "..index)
+    end
+
+    local M__index = function(instance, index) 
+      local proxy, impl, rx = object_manipulators(instance) 
+
+      --local hm,hp = rx:hasMethod(index), rx:hasProperty(index)
+
+      --print("R:", instance, proxy, impl, index, type(impl[index]))
+      ----print(index, rx:hasProperty(index), rx:hasMethod(index), rx:hasGetter(index))
+      if util.isSetter(index) and rx:hasMethod(index) and rx:hasProperty(util.toProperty(index)) then
+        --print("S + P", index)
+        return function(instance,val) impl[index](impl,val) end
+      elseif rx:hasGetter(index) and rx:hasProperty(index) then 
+        --print("G + P")
+        error('undefined behavior, there is defined both property and getter for: '.. index)
+      elseif rx:hasProperty(index) or rx:hasMethod(index) then -- get direct property/method
+        --print("D")
+        return impl[index]
+      elseif not rx:hasProperty(index) and rx:hasGetter(index) then
+        local getter = util.toGetter(index)
+        --print("P -> G", index, getter)
+        return impl[getter](impl)
+      elseif not rx:hasMethod(index) and rx:hasProperty(util.toProperty(index)) then
+        local property = util.toProperty(index)
+        if util.isSetter(index) then
+          --print("S -> P", index, property)
+          return function(instance,val) impl[property] = val end
+        else 
+          --print("G -> P", index, property)
+          return function() return impl[property] end
+        end
+      elseif index == 'is_a' then
+        return function(instance,class) return is_a(instance, class) end
+      end
+      error("read unknown attribute: "..index)
+    end
 
 
 local object_proxy = function(impl, parent)
@@ -82,53 +166,9 @@ local object_proxy = function(impl, parent)
   if parent_mt then setmetatable(impl, { __index = parent_mt.impl }) end
 
   local proxy = {
-    __index = function(instance, index) 
-      local proxy, impl, rx = object_manipulators(instance) 
-      print("R:", instance, proxy, impl, index, type(impl[index]))
-      --print(index, rx:hasProperty(index), rx:hasMethod(index), rx:hasGetter(index))
-      if util.isSetter(index) and rx:hasMethod(index) and rx:hasProperty(util.toProperty(index)) then
-        print("S + P", index)
-        return function(instance,val) impl[index](impl,val) end
-      elseif rx:hasGetter(index) and rx:hasProperty(index) then 
-        print("G + P")
-        error('undefined behavior, there is defined both property and getter for: '.. index)
-      elseif rx:hasProperty(index) or rx:hasMethod(index) then -- get direct property/method
-        print("D")
-        return impl[index]
-      elseif not rx:hasProperty(index) and rx:hasGetter(index) then
-        local getter = util.toGetter(index)
-        print("P -> G", index, getter)
-        return impl[getter](impl)
-      elseif not rx:hasMethod(index) and rx:hasProperty(util.toProperty(index)) then
-        local property = util.toProperty(index)
-        if util.isSetter(index) then
-          print("S -> P", index, property)
-          return function(instance,val) impl[property] = val end
-        else 
-          print("G -> P", index, property)
-          return function() return impl[property] end
-        end
-      elseif index == 'is_a' then
-        return function(instance,class) return is_a(instance, class) end
-      end
-      error("read unknown attribute: "..index)
-    end,
 
-    __newindex = function(instance, index, value)
-      local proxy, impl, rx = object_manipulators(instance) 
-      print("W:", instance, proxy, impl, index, type(impl[index]), value)
-      if rx:hasSetter(index) then
-        print("S")
-        local setter = util.toSetter(index)
-        impl[setter](impl,value)
-        return
-      elseif impl[index] ~= nil then -- get direct property/method
-        print("D")
-        impl[index] = value
-        return 
-      end
-      error("write unknown attribute: "..index)
-    end,
+    __newindex = M__newindex,
+    __index = M__index,
 
     __call = function(class, ...)
       local instance = object(class,{})
@@ -154,12 +194,12 @@ local object_proxy = function(impl, parent)
         end
       end
 
-      print("I:",class, instance, ">", arg)
-
+      --print("I:",class, instance, ">", arg)
       return instance
     end,
 
     impl = impl,
+    reflection = object_reflection(impl),
   }
 
 
@@ -191,7 +231,7 @@ object = function(parent, impl)
   impl = impl or {}
 
   local inst = setmetatable({}, object_proxy(impl, parent))
-  print("C:",impl,inst,getmetatable(inst), parent)
+  --print("C:",impl,inst,getmetatable(inst), parent)
   return inst
 end
 

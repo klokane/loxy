0bfe67f770c68d1e6ec40dfc3337589bae3fac10
Jiri Kratochvil <kratochvil@klok.cz>
2014-01-06 00:14:23 +0100

    * add marhallers into signal implementation

--- signal.lua
+++ signal.lua
@@ -1,17 +1,63 @@
 require 'callback'
 
-function signal()
+--[[
+-- Marshaller has defined interface (contract)
+--   m = marshaller() -> create marshaller instance
+--   - m:add() - collect result if invocation
+--   - m:res() - return marshallerd value(s)
+--
+--   preddefind two marshallers:
+--   last - return last value
+--   table - return all values collected in table
+--
+--   signal receive as param type of marshaller,
+--   if send - it will use marshaller.last
+--]]
+
+local t_insert = table.insert
+
+marshaller = {
+  last = function()
+    return {
+      val = nil,
+      add = function(self,val)
+        self.val = val
+      end,
+      res = function(self)
+        return self.val
+      end,
+    }
+  end,
+
+  table = function()
+    return {
+      val = {},
+      add = function(self,val)
+        t_insert(self.val,val)
+      end,
+      res = function(self)
+        return self.val
+      end,
+    }
+  end,
+}
+
+function signal(m)
+  m = m or marshaller.last
   return setmetatable({
     cb_ = {},
+    marshaller = m,
     
     connect = function(self, ...)
       table.insert(self.cb_, callback(...))
     end,
 
     emit = function(self, ...)
+      local m = self.marshaller()
       for i = 1,#self.cb_ do
-        self.cb_[i]:invoke(...)
+        m:add(self.cb_[i]:invoke(...))
       end
+      return m:res()
     end,
 
     },{
--- spec/signal_spec.lua
+++ spec/signal_spec.lua
@@ -34,3 +34,35 @@ describe('testing signal functionality', function()
   end)
 
 end)
+
+describe('testing preddefined marshallers functionality', function()
+
+  it('"last" returns last value',function()
+    local s = signal()
+    s:connect(function() return 1 end)
+    s:connect(function() return 2 end)
+    s:connect(function() return 3 end)
+    -- last invocation value is returned
+    assert.is.equal(3, s())
+
+    -- next emit return last value again
+    assert.is.equal(3, s())
+
+    -- add next callback
+    s:connect(function() return 4 end)
+    -- last value is now new callback
+    assert.is.equal(4, s())
+  end)
+
+  it('"table" returns last value',function()
+    local s = signal(marshaller.table)
+    s:connect(function() return 1 end)
+    s:connect(function() return 2 end)
+    s:connect(function() return 3 end)
+    assert.is.same({1,2,3}, s())
+
+    s:connect(function() return 4 end)
+    assert.is.same({1,2,3,4}, s())
+  end)
+
+end)
